f784093c16f5f49d9f8ba421fbdcfc3e1949fdce
root <root@sehele16732.ptr1.ru>
2023-10-18 12:48:18 +0300

    Changing Launcher, and theme.

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,17 +15,28 @@ function Header() {
 
     return (
       <>
-       <header className="header">
-                <a className="header--logo" href="#0">
-                        <img src={logo} alt="Global" width={55}/>
-                        {/* <p>Tonamento</p> */}
-                </a>
-               <button className="header--cta cta">Hire Us</button>
-               <div className="header--nav-toggle">
-                    <span></span>
-               </div>
-        </header>
-        <nav className="l-side-nav">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+            <img id="tona-logo" src={logo} width={50}/>
+            <div className="container px-5">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i className="bi-list"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                        <li className="nav-item"><a className="nav-link me-lg-3" href="#features">Whitepaper</a></li>
+                        <li className="nav-item"><a className="nav-link me-lg-3" href="#download">Roadmap</a></li>
+                    </ul>
+                    <button className="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
+                        <span className="d-flex align-items-center">
+                            <i className="bi-chat-text-fill me-2"></i>
+                            <span className="small">Open Console</span>
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </nav>
+         <nav className="l-side-nav">
           <div id='nav-cover'></div>
            <ul className="side-nav">
                <li className={activeKey === "home" ? "is-active" : ""} data-key="home" onClick={handleClick}><span>Home</span></li>
